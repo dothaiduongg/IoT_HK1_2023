887627f103e0d9e546a6cf84e5b08f19cced480e
Do Thai Duong <114820514+dothaiduongg@users.noreply.github.com>
2023-11-08 13:06:22 +0700

    update code

--- Lab_3/Lab3_esp32_ThingSpeak/Lab3_esp32_ThingSpeak.ino
+++ Lab_3/Lab3_esp32_ThingSpeak/Lab3_esp32_ThingSpeak.ino
@@ -1,29 +1,34 @@
+
+//khai bao thu vien wifi va cam bien
 #include <WiFi.h>
-#include "ThingSpeak.h"
+#include <ThingSpeak.h>
 #include <DHT.h>
 
-#define WIFI_SSID "Duongc3kt"
-#define WIFI_PASSWORD "ouduorone110403"
+#define WIFI_SSID "Xuan Sang"           //ten Wifi hien tai
+#define WIFI_PASSWORD "68686868"        //password
 
-#define SECRET_CH_ID 2332495
-#define SECRET_WRITE_APIKEY "4RM0O1IIQFLCTIUI" 
+#define SECRET_CH_ID 2334577                          //Channel ID
+#define SECRET_WRITE_APIKEY "S2YOVFS3QCHLEWC9"        //Write API key
 
 
-#define DHTPIN 14 
+#define DHTPIN 14                       //Gan chan cho esp
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
 WiFiClient client;
 
-unsigned long myChannelNumber = SECRET_CH_ID;
-const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+unsigned long myChannelNumber = SECRET_CH_ID;             //gan bien cho Channel ID
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;         //gan bien cho Write API key
 
-unsigned long dataMillis = 0;
+unsigned long dataMillis = 0;                   //dieu khien delay
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);                          //khai bao toc do serial
   delay(1000);
+
+
+  //kiem tra ket noi wifi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
@@ -31,10 +36,15 @@ void setup() {
   Serial.print(".");
   delay(500);
   }
+
+
+  //
   Serial.println();
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
+
+  //khoi tao thingspeak
   ThingSpeak.begin(client);
 }
 
@@ -46,9 +56,7 @@ void loop() {
     if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
-
     }
-
     Serial.print("Nhiet do: ");
     Serial.print(t);
     Serial.print("*C ");
@@ -56,12 +64,17 @@ void loop() {
     Serial.print(h);
     Serial.println("% ");
 
-  ThingSpeak.setField(1, t);
+      //ket qua hien thi tren man hinh ung voi thu tu bieu do tren thingspeak
+  ThingSpeak.setField(1, t);                       
   ThingSpeak.setField(2, h);
-  if (millis() - dataMillis > 20000)
+
+                                     
+  if (millis() - dataMillis > 20000)        
   {
-  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-   if(x == 500){
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);     //ghi du lieu dc set len giao dien channel
+   if(x == 200){              
+    //ham so sanh x == 200 vi trong phan write data cua mathwork cho giao thuc http, trang thai gui tin hieu thanh cong la 200, neu gui khac gia tri 200 thi tren thingspeak khong nhan gia tri
+    //Link: https://ch.mathworks.com/help/thingspeak/writedata.html 
    Serial .println("Channel update successful.");
   } 
     else {
